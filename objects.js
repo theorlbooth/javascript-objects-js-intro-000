7a6aa2bf1979917f22790e0e16e16887dceedd24
Theo Booth <theorlbooth@googlemail.com>
2020-02-26 17:45:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ function updatePlaylist(playlist, artist, song){
   playlist[artist] = song
   return playlist
 }
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
